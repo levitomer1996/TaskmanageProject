cd12460abe29ec7f817331c955dd6fe5887e04af
levit <levitomer1996@gmail.com>
2024-08-10 09:45:56 +0300

    Set task user id fixed

--- app/src/main/java/Controller/TaskDataBaseManager.java
+++ app/src/main/java/Controller/TaskDataBaseManager.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import android.util.Log;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -20,30 +22,43 @@ public class TaskDataBaseManager {
     public TaskDataBaseManager() {
         db = FirebaseFirestore.getInstance();
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser == null) {
+            // Handle the case where the user is not authenticated
+            Log.e(TAG, "User not authenticated");
+        }
     }
 
+
     public void getAll(final TaskDataCallback callback) {
-        db.collection(TASKS_NODE)
-                .whereEqualTo("user_id", currentUser.getUid())
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        List<Task> tasks = new ArrayList<>();
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            Task taskObj = document.toObject(Task.class);
-                            tasks.add(taskObj);
+        if (currentUser != null) {
+            db.collection(TASKS_NODE)
+                    .whereEqualTo("user_id", currentUser.getUid())
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            List<Task> tasks = new ArrayList<>();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Task taskObj = document.toObject(Task.class);
+                                tasks.add(taskObj);
+                            }
+                            Log.d(TAG, "Fetched " + tasks.size() + " tasks");
+                            callback.onSuccess(tasks);
+                        } else {
+                            Log.e(TAG, "Error getting documents: ", task.getException());
+                            callback.onFailure(task.getException());
                         }
-                        callback.onSuccess(tasks);
-                    } else {
-                        callback.onFailure(task.getException());
-                    }
-                });
+                    });
+        } else {
+            Log.e(TAG, "User not authenticated");
+            callback.onFailure(new Exception("User not authenticated"));
+        }
     }
 
 
     public void save(Task task) {
         String taskId = db.collection(TASKS_NODE).document().getId();
         task.setUid(taskId);
+        task.setUser_id(currentUser.getUid());
         db.collection(TASKS_NODE).document(taskId).set(task);
     }
 
